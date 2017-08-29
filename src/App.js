b307a51de208df0ea43f30a2c290444b434d15f3
sivaramaks <sivaramaks@ncs.com.sg>
2017-08-29 18:58:11 +0800

    search control added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import Info from './components/Info';
 import './App.css';
 
 class App extends Component {
-  constructor () {
-        super();
+  constructor(props) {
+        super(props);
+
+        this.filterName = this.filterName.bind(this);
+
         this.state = {
             title: "React App - Countries Info",
             subtitle:"Collection of informations are here for all the countires.",
@@ -14,6 +17,17 @@ class App extends Component {
         }
     }
 
+    filterName(event) {
+
+      var updatedList = this.state.info;
+
+      updatedList = updatedList.filter(function(item){ 
+        return item.name.toLowerCase().search(
+          event.target.value.toLowerCase()) !== -1;
+      });
+
+      this.setState({info: updatedList});
+    }
     
     componentDidMount () {
         const apiUrl = 'https://restcountries.eu/rest/v2/all';
@@ -25,8 +39,10 @@ class App extends Component {
           this.setState({
               info: data
           });
-            console.log(data);
-        })
+          //console.log(data);
+        });
+
+        
     }
 
   render() {
@@ -34,9 +50,12 @@ class App extends Component {
       <div className="App">
         <Header title={this.state.title} secondtitle={this.state.subtitle}/>
         <div className="container-fluid">
-          
+              <div className="row">
+                <div className="col-lg-12 col-md-12 col-sm-12">
+                  <input type="text" placeholder="Search here" value={this.state.search}  className="form-control" onChange={this.filterName}/>
+                </div>
+              </div>
               <Info info={this.state.info} />
-          
         </div>
       </div>
     );
